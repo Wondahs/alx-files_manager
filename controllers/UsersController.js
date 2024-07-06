670fae7e82ad0e97a53fb430864df5966c2acf59
Wonders Victor <wondersprince1@gmail.com>
2024-07-06 08:42:21 +0100

    Fixes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -33,7 +33,7 @@ class UsersController {
   static async getMe(req, res) {
     const token = req.headers['x-token'];
     if (!token) {
-      return res.status(401).send('Unauthorized');
+      return res.status(401).json({ error: 'Unauthorized' });
     }
     const userId = await redisClient.get(`auth_${token}`);
 
